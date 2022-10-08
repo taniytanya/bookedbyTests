7568c87c84909f65842343fedd4e9129d9633a50
taniytanya <taniytanya@gmail.com>
2022-10-08 21:39:59 +0300

    change testbase, readme

--- src/test/java/bookedBy/helpers/DriverUtils.java
+++ src/test/java/bookedBy/helpers/DriverUtils.java
@@ -31,19 +31,8 @@ public class DriverUtils {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
     }
 
-    public static URL getVideoUrl(String sessionId) {
-        String videoUrl = Project.config.videoStorage() + sessionId + ".mp4";
 
-        try {
-            return new URL(videoUrl);
-        } catch (MalformedURLException e) {
-            LOGGER.warn("[ALLURE VIDEO ATTACHMENT ERROR] Wrong test video url, {}", videoUrl);
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getConsoleLogs() { // todo refactor
+    public static String getConsoleLogs() {
         return String.join("\n", Selenide.getWebDriverLogs(BROWSER));
     }
 }
