f2c4f051c623d67d29f416b45d889dece4495fa9
taniytanya <taniytanya@gmail.com>
2022-10-09 14:05:23 +0300

    change readme

--- src/test/java/bookedBy/tests/TestBase.java
+++ src/test/java/bookedBy/tests/TestBase.java
@@ -1,20 +1,15 @@
 package bookedBy.tests;
 
-import bookedBy.config.Project;
-import bookedBy.helpers.AllureAttachments;
+import bookedBy.helpers.Attach;
 import bookedBy.helpers.DriverSettings;
-import bookedBy.helpers.DriverUtils;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.junit5.AllureJunit5;
 import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 
 @ExtendWith({AllureJunit5.class})
@@ -31,13 +26,10 @@ public class TestBase {
 
     @AfterEach
     public void afterEach() {
-        String sessionId = DriverUtils.getSessionId();
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
 
-        AllureAttachments.addScreenshotAs("Last screenshot");
-        AllureAttachments.addPageSource();
-        AllureAttachments.addBrowserConsoleLogs();
-        AllureAttachments.addVideo();
-        Selenide.closeWebDriver();
     }
-
 }
